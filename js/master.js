6366b003e7cfc21cf1a717fc43f3c09185da5768
Mahmoud Mostafa <mahmoud309mostafa@gmail.com>
2022-08-08 11:15:04 +0200

    Finished Random background options

--- js/master.js
+++ js/master.js
@@ -1,3 +1,28 @@
+//Random background option
+let option = true;
+
+// Variable To control the internval
+let interval;
+
+// Check if there's randombackground in local storage
+let bgLocalStorage = localStorage.getItem("option");
+if (bgLocalStorage !== null) {
+  if (bgLocalStorage === "true") {
+    option = true;
+  } else {
+    option = false;
+  }
+  //Remove Active Class from All Spans
+  document.querySelectorAll(".random-background span").forEach((ele) => {
+    ele.classList.remove("active");
+  });
+  if (bgLocalStorage === "true") {
+    document.querySelector(".random-background .yes").classList.add("active");
+  } else {
+    document.querySelector(".random-background .no").classList.add("active");
+  }
+}
+
 //check if there's local storage color option
 let mainColors = localStorage.getItem("color-option");
 if (mainColors !== null) {
@@ -11,6 +36,26 @@ if (mainColors !== null) {
     }
   });
 }
+
+// Change Active Class of random background
+const randomBg = document.querySelectorAll(".random-background span");
+randomBg.forEach((e) => {
+  e.addEventListener("click", (ele) => {
+    ele.target.parentElement.querySelectorAll(".active").forEach((element) => {
+      element.classList.remove("active");
+    });
+    ele.target.classList.add("active");
+    if (ele.target.dataset.background === "yes") {
+      option = true;
+      randomizeImgs();
+      localStorage.setItem("option", option);
+    } else {
+      option = false;
+      clearInterval(interval);
+      localStorage.setItem("option", option);
+    }
+  });
+});
 
 // Toggle Spin Class On Icon
 document.querySelector(".toggle-settings .fa-gear").onclick = function () {
@@ -45,8 +90,15 @@ let landingPage = document.querySelector(".landing-page");
 let imgs = ["img-1.jpg", "img-2.jpg", "img-3.jpg", "img-4.jpg"];
 
 //Get Random Number
-setInterval(() => {
-  let randomNum = Math.floor(Math.random() * imgs.length);
-  //Change background image url
-  landingPage.style.backgroundImage = 'url("./imgs/' + imgs[randomNum] + '")';
-}, 10000);
+const randomizeImgs = () => {
+  if (option === true) {
+    interval = setInterval(() => {
+      let randomNum = Math.floor(Math.random() * imgs.length);
+      //Change background image url
+      landingPage.style.backgroundImage =
+        'url("./imgs/' + imgs[randomNum] + '")';
+    }, 1000);
+  }
+};
+
+randomizeImgs();
